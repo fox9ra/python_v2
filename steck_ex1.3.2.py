d3bab79723f0311991103bb5798205bdff53a507
bagriy <bagriy@complete.ru>
2019-03-05 16:43:45 +0300

    upd + konspekt

--- steck_ex1.3.2.py
+++ steck_ex1.3.2.py
@@ -17,3 +17,16 @@ def g(a):
 print("\nMODULE")
 g(f)
 '''
+def s(a, *vs, b=10):
+   res = a + b
+   for v in vs:
+       res += v
+   return res
+print(s(0, 0, 31))
+print(s(11, 10))
+print(s(11, 10, b=10))
+print(s(21))
+print(s(11, b=20))
+print(s(b=31))
+print(s(11, 10, 10))
+print(s(5, 5, 5, 5, 1))
